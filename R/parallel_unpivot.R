8af0d987d56ce303446e5aae2d8c3a543c22a9fb
rodrigomalagon <rodrigo.malagon.rodriguez@gmail.com>
2024-07-26 03:42:18 +0200

    Initial commit

--- R/parallel_unpivot.R
+++ R/parallel_unpivot.R
@@ -1,6 +1,6 @@
 parallel_unpivot <- function(raw_df,start_pattern1,start_pattern2,new_name1,new_name2,time_name,regex_group){
   df <- tidyr::pivot_longer(raw_df,
-                            cols = starts_with(start_pattern1) | starts_with(start_pattern2),
+                            cols = tidyr::starts_with(start_pattern1) | tidyr::starts_with(start_pattern2),
                             names_to = c('.value',time_name),
                             names_pattern = regex_group
   )
